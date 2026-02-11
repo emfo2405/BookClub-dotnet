f116696e5ac04890446f9cf7e81eee57d81705f7
emfo2405 <emfo2405@student.miun.se>
2026-02-11 10:23:42 +0100

    Databas uppdaterad

--- obj/Debug/net10.0/BookClub.AssemblyInfo.cs
+++ obj/Debug/net10.0/BookClub.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookClub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0749b8ff63f0cba9a9077270139e11d2b74b825")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e03154bfaaa06ca6ce8bc463bd677cb17298e97")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookClub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookClub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
