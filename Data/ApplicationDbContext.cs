067ac0d202b6d6377b720e31ad49d01136c880a1
emfo2405 <emfo2405@student.miun.se>
2026-02-11 09:38:20 +0100

    DbContext skapad

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,8 +1,22 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BookClub.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookClub.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+public class ApplicationDbContext : IdentityDbContext
 {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+    {
+        
+    }
+
+   public DbSet<AuthorModel>? Author { get; set; } 
+   public DbSet<BookModel>? Book { get; set; }
+   public DbSet<ChapterModel>? Chapter { get; set; }
+   public DbSet<DiscussionModel>? Discussion { get; set; }
+   public DbSet<ReviewModel>? Review { get; set; }    
 }
+
+
