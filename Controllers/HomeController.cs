6a04e7c019d49dcc3b155454bd2fb24b1976584c
emfo2405 <emfo2405@student.miun.se>
2026-02-24 15:11:34 +0100

    Den senast tillagda boken visas på startsidan

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,15 +1,30 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using BookClub.Models;
+using BookClub.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookClub.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index()
+    private readonly ApplicationDbContext _context;
+
+    public HomeController(ApplicationDbContext context)
     {
-        return View();
+        _context = context;
     }
+
+    public async Task<IActionResult> Index()
+    {
+        var currentBook = await _context.Book
+            .Include(b => b.Author)
+            .OrderByDescending(b => b.Id)
+            .FirstOrDefaultAsync();
+
+            return View(currentBook);
+    }
+
 
     public IActionResult Privacy()
     {
