ac6630498dc22b4685216a6558aaade0ecd30038
emfo2405 <emfo2405@student.miun.se>
2026-03-12 12:35:42 +0100

    CSS tillagt för delete-view för en författare

--- obj/Debug/net10.0/BookClub.AssemblyInfo.cs
+++ obj/Debug/net10.0/BookClub.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookClub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1375275d63facb1a0f012e1d402da3d52f7d53ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1be74dba472c4d64ff63d31622e00458f278da50")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookClub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookClub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
