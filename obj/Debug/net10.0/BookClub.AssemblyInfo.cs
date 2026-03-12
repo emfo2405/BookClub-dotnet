40413cb56ca478a3a6e06957617d662ae58b1bfc
emfo2405 <emfo2405@student.miun.se>
2026-03-12 19:10:27 +0100

    Manage account sida scaffoldad fram för att kunna göra designval

--- obj/Debug/net10.0/BookClub.AssemblyInfo.cs
+++ obj/Debug/net10.0/BookClub.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookClub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd99c5cc07f711bd6984ad065a58fd6aab61250b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee28873e314c4b121b330ddcd7f9d84c171f9678")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookClub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookClub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
