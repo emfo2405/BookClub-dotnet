79349a216fcf32f4aaec0f1bb14873f42cb4de95
emfo2405 <emfo2405@student.miun.se>
2026-04-09 19:32:30 +0200

    Docker-fil skapad för railway publicering

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Bygg
+FROM mcr.microsoft.com/dotnet/sdk:10.0 AS build
+WORKDIR /app
+
+# Kopiera och återställ beroenden
+COPY . ./
+RUN dotnet publish -c Release -o /out
+
+# Kör
+FROM mcr.microsoft.com/dotnet/aspnet:10.0
+WORKDIR /app
+COPY --from=build /out ./
+
+ENTRYPOINT ["dotnet", "BookClub.dll"]
--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString, sqlOptions =>
+    options.UseNpgsql(connectionString, sqlOptions =>
     {
         sqlOptions.EnableRetryOnFailure(
             maxRetryCount: 5,
